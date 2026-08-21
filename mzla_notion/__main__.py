9724dfa4e8f23416acf928579ff189b37102c9fb
Philipp Kewisch <mozilla@kewis.ch>
2026-08-21 10:17:49 +0200

    Fix entry point script

--- mzla_notion/__main__.py
+++ mzla_notion/__main__.py
@@ -3,7 +3,14 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import asyncio
-import sys
-from .cli import async_main
 
-sys.exit(asyncio.run(async_main()))
+from mzla_notion.cli import async_main
+
+
+def main():
+    """Run the mzla-notion command line interface."""
+    return asyncio.run(async_main())
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
